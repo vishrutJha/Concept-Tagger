5a61b747f5a37bda963984210db557b5e1f92564
vishrutJha <jha.vishrut@gmail.com>
2014-06-03 23:21:41 +0530

    Tags added to search

--- Front End/tagsearch.php
+++ Front End/tagsearch.php
@@ -6,16 +6,22 @@ $coll=$db->Domains;
 
 if(isset($_REQUEST["q"])){
 	$query = $_REQUEST["q"];
-	$cur = $coll->find(array("Domain" => new MongoRegex("/$query/i")));
-
+	
 	$tags = explode(" ", $query);
+	$query = $tags[0];
+	$cur = $coll->find(array("Domain" => new MongoRegex("/$query/i")));
+	$cur2 = $coll->find(array(
+		    'tags' => array('$in' => $tags)
+	    ));
 } else {
 	$cur = $coll->find();
 }
 
 $result = array();
+$dedup = array();
 
-foreach($cur as $item){
+foreach($cur2 as $item){
+    if(!isset($dedup[$item["Domain"]])){
 	$par_res = array();
 	$par_res["domain"] = $item["Domain"];
 	if(isset($item["tags"])){
@@ -24,6 +30,22 @@ foreach($cur as $item){
 		$par_res["tags"] = [];
 	}
 	$result[] = $par_res;
+	$dedup[$item["Domain"]] = 1;
+    } 
+}
+
+foreach($cur as $item){
+    if(!isset($dedup[$item["Domain"]])){
+	$par_res = array();
+	$par_res["domain"] = $item["Domain"];
+	if(isset($item["tags"])){
+		$par_res["tags"] = $item["tags"];
+	} else {
+		$par_res["tags"] = [];
+	}
+	$result[] = $par_res;
+	$dedup[$item["Domain"]] = 1;
+    } 
 }
 
 echo json_encode($result);
